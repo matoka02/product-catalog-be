e9267165c0d83f39167c2da46d689d7c1530d790
Anastasiia <120492521+matoka02@users.noreply.github.com>
2024-06-22 22:51:40 +0300

    add models
    fix server.ts

--- src/server.ts
+++ src/server.ts
@@ -10,11 +10,8 @@ mongooseConnect();
 export const app = express();
 
 app.use(cors({ origin: process.env.CLIENT_URL })).use(express.json());
-<<<<<<< Updated upstream
-=======
 
 app.get('/', async (_, responce) => {
   responce.status(200).send('Hello from Anastasiia')
 })
 
->>>>>>> Stashed changes
