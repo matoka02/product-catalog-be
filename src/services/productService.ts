6c8ddce4a8fb6c1809c7dfe254880a507cfccc08
Anastasiia <120492521+matoka02@users.noreply.github.com>
2024-06-23 20:04:57 +0300

    update
    - get product by type and recommended;
    - add routes;

--- src/services/productService.ts
+++ src/services/productService.ts
@@ -1,3 +1,4 @@
+import { populate } from 'dotenv';
 import { Product } from '../models/productModel';
 
 type Params = {
@@ -10,14 +11,14 @@ const getAll = async ({ page, perPage, sortBy }: Params) => {
   const offset = perPage * (page - 1);
   const order = sortBy === 'Newest' ? 'desc' : 'asc';
 
-  const productsCollection = await Product
-    .find()
+  const productsCollection = await Product.find()
     .populate('category')
     .populate('description')
     .sort({ updateAt: order, })
     .skip(offset)
     .limit(perPage);
-  const productsCollectionCount = await Product.count();
+  // const productsCollectionCount = await Product.count();
+  const productsCollectionCount = await Product.countDocuments();
 
   const data = {
     totalProducts: productsCollectionCount,
@@ -28,12 +29,76 @@ const getAll = async ({ page, perPage, sortBy }: Params) => {
 }
 
 const getOne = async (productId: string) => {
-  const foundProduct = await Product
-    .findById(productId)
+  const foundProduct = await Product.findById(productId)
     .populate('description')
     .populate('category');
 
   return foundProduct;
 }
 
-export default { getAll, getOne };
+const getFiltered = async (query: string) => {
+  const products = await Product.find({
+    name: { $regex: query, $options: 'i' }
+  })
+    .populate('category')
+    .populate('description');
+
+  return products;
+}
+
+const allProducts = () => {
+  return Product.find()
+    .populate('category')
+    .populate('description');
+}
+
+const getRandom = async (limit: number) => {
+  const products = await allProducts();
+  const randomProducts = [];
+  const indexes: number[] = [];
+  let i = 0;
+
+  while (i < limit) {
+    const randomIndex = Math.floor(Math.random() * products.length);
+
+    if (indexes.includes(randomIndex)) {
+      continue;
+    } else {
+      indexes.push(randomIndex);
+      i++;
+      randomProducts.push(products[randomIndex]);
+    }
+  };
+
+  return randomProducts;
+}
+
+const getByType = async (type: string) => {
+  switch (type) {
+    case 'phones': {
+      return Product.find()
+        .populate({
+          path: 'category',
+          match: {
+            name: 'phones',
+          },
+        })
+        .populate('description');
+    }
+
+    case 'tablets': {
+      return ['tablets'];
+    }
+
+    case 'accessories': {
+      return ['accessories'];
+    }
+
+    default:
+      return [];
+  }
+}
+
+
+
+export default { getAll, getOne, getFiltered, getRandom, getByType };
