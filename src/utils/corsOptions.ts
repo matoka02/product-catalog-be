9bfa0d660fad2437777b01bf19483b091c5f56b0
Anastasiia <120492521+matoka02@users.noreply.github.com>
2024-07-27 16:06:43 +0300

    update CORS

--- src/utils/corsOptions.ts
+++ src/utils/corsOptions.ts
@@ -1,16 +1,14 @@
 import { CorsCallback } from '../types/CorsCallback';
 
-const whiteList = [
-  'http://localhost:4000',
-];
+const whitelist = ['http://localhost:4000', 'https://ghostmatoka.netlify.app'];
 
 export const corsOptions = {
-  origin: ''
-  //   (origin: string, callback: CorsCallback) => {
-  //   if (whitelist.indexOf(origin) !== -1) {
-  //     callback(null, true);
-  //   } else {
-  //     callback(new Error('Not allowed by CORS'));
-  //   }
-  // },
+  // origin: ''
+  origin: (origin: string, callback: CorsCallback) => {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
 };
